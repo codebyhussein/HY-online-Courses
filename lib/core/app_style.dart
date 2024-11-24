ca87bf6c4606ede57f5d48733badfc80b23967b3
hussein_salah <husseinsalah2528@gmail.com>
2024-11-25 00:19:34 +0200

    create core file

--- lib/core/app_style.dart
+++ lib/core/app_style.dart
@@ -13,4 +13,13 @@ class AppStyle {
     fontWeight: FontWeight.w600,
     color: Colors.grey[700], // Change color as needed
   );
+
+  static TextStyle style14Semibold = const TextStyle(
+    color: Color(0xFF01261C),
+    fontSize: 14,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.bold,
+    height: 0,
+    letterSpacing: 0.56,
+  );
 }
--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color kmainColor = const Color(0x00dbddde);
+  static Color secondaryColor = const Color(0xFFDADADA);
 }
