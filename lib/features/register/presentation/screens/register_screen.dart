0c5744e223ca7caea7f61ff26ad67e1018bbaded
hussein_salah <husseinsalah2528@gmail.com>
2024-11-25 20:45:25 +0200

    add structure for register screen

---
+++ lib/features/register/presentation/screens/register_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: const Text('register-screen'),
+        ),
+      ),
+    );
+  }
+}
